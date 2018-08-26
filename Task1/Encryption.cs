bb5efad1e63cdbbe1ff83d83471b6829158ad29d
Gomotso <gomotsogpm@gmail.com>
2018-08-27 00:59:16 +0200

    Add project files.

---
+++ Task1/Encryption.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Encryption
+    {
+        private AesCryptoServiceProvider cryptoProvider;
+
+        public Encryption()
+        {
+            cryptoProvider = new AesCryptoServiceProvider
+            {
+                KeySize = 256, // setting he 
+                BlockSize = 128
+            };
+
+            cryptoProvider.GenerateIV(); // generate a random initialization vector to use 
+            cryptoProvider.GenerateKey();// generate a random key to use 
+            cryptoProvider.Mode = CipherMode.ECB; 
+            cryptoProvider.Padding = PaddingMode.PKCS7;
+        }
+
+        public string Encrypt(string plainText)
+        {
+            ICryptoTransform transform = cryptoProvider.CreateEncryptor();// creates an AES encryptor object using the generated key and initialization vector
+            byte[] encryptedBytes = transform.TransformFinalBlock(Encoding.ASCII.GetBytes(plainText), 0, plainText.Length);//enctrypt the text
+            
+            return Convert.ToBase64String(encryptedBytes);// return it
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            ICryptoTransform transform = cryptoProvider.CreateDecryptor();// creates an AES Decryptor object using the generated key and initialization vector
+            byte[] encryptedBytes = Convert.FromBase64String(cipherText);
+            byte[] decryptedBytes = transform.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);// converts encrypted text to plain text
+
+            return Encoding.ASCII.GetString(decryptedBytes);
+        }
+    }
+}
---
+++ Task1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        private readonly Encryption _encryption;
+        public Form1()
+        {
+            InitializeComponent();
+            _encryption = new Encryption();// initialize global variable 
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();// clear the textbox before text is placed (assigned) into it
+            textBox2.Text = _encryption.Encrypt(textBox1.Text);// assign encrypted text to the textbox
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();// clear the textbox before text is placed (assigned) into it
+            textBox1.Text = _encryption.Decrypt(textBox2.Text);// assign encrypted text to the textbox
+        }
+    }
+}
